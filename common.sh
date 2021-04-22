fbb363c613f22caddc32095332ee27d34367925d
KalanaDananjaya <kalana.16@cse.mrt.ac.lk>
2021-04-22 21:35:51 +0530

    Add a common script to handle cli commands

---
+++ common.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Starting script for VoBox
+# v0.1
+# kwijethu@cern.ch
+
+
+ldapHostname="alice-ldap.cern.ch"
+ldapPort="8389"
+hostname="voboxalice8.cern.ch"
+farmHome="/home/kalana/MonaLisa" # MonAlisa package extracted location
+
+source ./jalien-ce.sh
+source ./monalisa.sh
+
+if [[ $1 = "monalisa" ]]
+    then
+        run_monalisa $2 $ldapHostname $ldapPort $hostname $farmHome
+        
+    elif [[ $1 = "ce" ]]
+    then
+        run_ce $2
+        
+    elif [[ $1 = "help" ]]
+    then
+        echo "jalien-vobox <Command> <Service>"
+        echo "<Command> is one of: 'start', 'stop', 'restart', 'status', 'mlstatus', 'systemd'"
+	    echo "<Service> is one of: CE, MonaLisa (defaulting to both if not specified)"
+    else 
+        
+        
+fi
