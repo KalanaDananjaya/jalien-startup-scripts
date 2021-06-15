0392bfe2d59a0c4a81b7c5307626dd37c36380e0
Costin Grigoras <costing@gmail.com>
2021-06-15 09:33:34 +0200

    ML starting fixes

--- jalien-vobox.sh
+++ jalien-vobox.sh
@@ -9,7 +9,7 @@ ldapHostname="alice-ldap.cern.ch"
 ldapPort="8389"
 hostname=`hostname -f`
 
-dir=${JALIEN_SCRIPTS:-/cvmfs/alice.cern.ch/scripts}
+dir=`dirname -- "$0"`
 
 source $dir/jalien-ce.sh
 source $dir/monalisa.sh
